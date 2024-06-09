bea683c9d6498ae04963d4213be7e998a24e8329
Dheeja <jagadeshjaga385@gmail.com>
2024-06-09 22:36:47 +0530

    Zeon commit

--- bin/appmystery.ts
+++ bin/appmystery.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AppmysteryStack } from '../lib/appmystery-stack';
+import { APPMYSTERYStack } from '../lib/appmystery-stack';
 
 const app = new cdk.App();
-new AppmysteryStack(app, 'AppmysteryStack', {
+new APPMYSTERYStack(app, 'APPMYSTERYStack', {
   // Uncomment the following line if you need to specify account and region
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
--- lib/appmystery-stack.ts
+++ lib/appmystery-stack.ts
@@ -3,89 +3,69 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
-import * as ecr from 'aws-cdk-lib/aws-ecr';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 
-export class AppmysteryStack extends Stack {
+export class APPMYSTERYStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // Create VPC
+    // Create a VPC with public and private subnets
     const vpc = new ec2.Vpc(this, 'MyVpc', {
       maxAzs: 2,
+      natGateways: 1,
       subnetConfiguration: [
         {
-          subnetType: ec2.SubnetType.PUBLIC,
-          name: 'PublicSubnet',
           cidrMask: 24,
+          name: 'public-subnet',
+          subnetType: ec2.SubnetType.PUBLIC,
         },
         {
-          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-          name: 'PrivateSubnet',
           cidrMask: 24,
+          name: 'private-subnet',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
         },
       ],
     });
 
-    // Create ECS Cluster
+    // ECS Cluster
     const cluster = new ecs.Cluster(this, 'MyCluster', {
       vpc,
     });
 
-    // Create ECR Repository
-    const repository = new ecr.Repository(this, 'MyRepository');
-
-    // Deploy a sample ECR image to ECS tasks
-    const taskDefinition = new ecs.FargateTaskDefinition(this, 'MyTaskDefinition', {
-      memoryLimitMiB: 512, // 0.5 GB
-      cpu: 256, // 0.25 vCPU
-    });
-
-    const container = taskDefinition.addContainer('MyContainer', {
-      image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+    // Task Definition
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef', {
       memoryLimitMiB: 512,
+      cpu: 256,
     });
 
-    // Add port mappings to the container
+    // Container Definition
+    const container = taskDefinition.addContainer('sample-container', {
+      image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'sample-container' }),
+    });
+
     container.addPortMappings({
-      containerPort: 8080, // Example port, change if necessary
+      containerPort: 8080,
     });
 
-    // Create ECS Service
-    const service = new ecs.FargateService(this, 'MyService', {
+    // ECS Service with Application Load Balancer
+    const ecsService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
       cluster,
       taskDefinition,
-      desiredCount: 2,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-      },
+      publicLoadBalancer: true,
     });
 
-    // Create ALB
-    const alb = new elbv2.ApplicationLoadBalancer(this, 'MyALB', {
-      vpc,
-      internetFacing: true,
+    // Allow inbound traffic to ECS tasks on port 8080
+    ecsService.targetGroup.configureHealthCheck({
+      path: "/",
+      port: "8080"
     });
 
-    // Create Target Group
-    const targetGroup = new elbv2.ApplicationTargetGroup(this, 'MyTargetGroup', {
-      port: 8080,
-      vpc,
-      targetType: elbv2.TargetType.IP,
-    });
-
-    // Add Targets
-    targetGroup.addTarget(service);
-
-    // Create Listener
-    alb.addListener('Listener', {
-      port: 8080,
-      defaultTargetGroups: [targetGroup],
-    });
-
-    // Output the ALB DNS Name
-    new cdk.CfnOutput(this, 'ALBDNSName', {
-      value: alb.loadBalancerDnsName,
+    // Output the ALB DNS
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', {
+      value: ecsService.loadBalancer.loadBalancerDnsName,
     });
   }
 }
+
+
